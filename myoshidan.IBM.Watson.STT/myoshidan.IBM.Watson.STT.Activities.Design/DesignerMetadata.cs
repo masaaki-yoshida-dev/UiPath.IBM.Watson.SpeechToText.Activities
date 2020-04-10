5f8e26e34509fd2befcfec1e27453238fedf5da2
Masaaki Yoshida <ma.yoshida.dev@gmail.com>
2020-04-10 23:35:18 +0900

    Initial Commit Create UiPath Templated Project

---
+++ myoshidan.IBM.Watson.STT/myoshidan.IBM.Watson.STT.Activities.Design/DesignerMetadata.cs
@@ -0,0 +1,27 @@
+﻿using System.Activities.Presentation.Metadata;
+using System.ComponentModel;
+using System.ComponentModel.Design;
+using myoshidan.IBM.Watson.STT.Activities.Design.Designers;
+using myoshidan.IBM.Watson.STT.Activities.Design.Properties;
+
+namespace myoshidan.IBM.Watson.STT.Activities.Design
+{
+    public class DesignerMetadata : IRegisterMetadata
+    {
+        public void Register()
+        {
+            #region Setup
+
+            var builder = new AttributeTableBuilder();
+            builder.ValidateTable();
+
+            var categoryAttribute = new CategoryAttribute($"{Resources.Category}");
+
+            #endregion Setup
+
+
+
+            MetadataStore.AddAttributeTable(builder.CreateTable());
+        }
+    }
+}
---
+++ myoshidan.IBM.Watson.STT/myoshidan.IBM.Watson.STT.Activities/Properties/SharedResources.cs
@@ -0,0 +1,6 @@
+﻿namespace UiPath.Shared.Localization
+{
+    class SharedResources : myoshidan.IBM.Watson.STT.Activities.Properties.Resources
+    {
+    }
+}
