b2aece710629c0c39d9c7b959e70d36831c79a82
Masaaki Yoshida <ma.yoshida.dev@gmail.com>
2020-04-19 17:58:30 +0900

    Create Subfolder SpeechToText File and Streaming

--- myoshidan.IBM.Watson.STT/myoshidan.IBM.Watson.STT.Activities.Design/DesignerMetadata.cs
+++ myoshidan.IBM.Watson.STT/myoshidan.IBM.Watson.STT.Activities.Design/DesignerMetadata.cs
@@ -15,35 +15,38 @@ namespace myoshidan.IBM.Watson.STT.Activities.Design
             var builder = new AttributeTableBuilder();
             builder.ValidateTable();
 
-            var categoryAttribute = new CategoryAttribute($"{Resources.Category}");
+            var categoryFileAttribute = new CategoryAttribute($"{Resources.CategoryFile}");
+            var categoryStreamingAttribute = new CategoryAttribute($"{Resources.CategoryStreaming}");
 
             #endregion Setup
 
-
-            builder.AddCustomAttributes(typeof(SpeechToTextFileScope), categoryAttribute);
+            #region FileGroup
+            builder.AddCustomAttributes(typeof(SpeechToTextFileScope), categoryFileAttribute);
             builder.AddCustomAttributes(typeof(SpeechToTextFileScope), new DesignerAttribute(typeof(SpeechToTextFileScopeDesigner)));
             builder.AddCustomAttributes(typeof(SpeechToTextFileScope), new HelpKeywordAttribute(""));
 
-            builder.AddCustomAttributes(typeof(RecognizeAudioFile), categoryAttribute);
+            builder.AddCustomAttributes(typeof(RecognizeAudioFile), categoryFileAttribute);
             builder.AddCustomAttributes(typeof(RecognizeAudioFile), new DesignerAttribute(typeof(RecognizeAudioFileDesigner)));
             builder.AddCustomAttributes(typeof(RecognizeAudioFile), new HelpKeywordAttribute(""));
+            #endregion
 
-            builder.AddCustomAttributes(typeof(SpeechToTextStreamingScope), categoryAttribute);
+            #region Streaming
+            builder.AddCustomAttributes(typeof(SpeechToTextStreamingScope), categoryStreamingAttribute);
             builder.AddCustomAttributes(typeof(SpeechToTextStreamingScope), new DesignerAttribute(typeof(SpeechToTextStreamingScopeDesigner)));
             builder.AddCustomAttributes(typeof(SpeechToTextStreamingScope), new HelpKeywordAttribute(""));
 
-            builder.AddCustomAttributes(typeof(StartStreamingRecognize), categoryAttribute);
+            builder.AddCustomAttributes(typeof(StartStreamingRecognize), categoryStreamingAttribute);
             builder.AddCustomAttributes(typeof(StartStreamingRecognize), new DesignerAttribute(typeof(StartStreamingRecognizeDesigner)));
             builder.AddCustomAttributes(typeof(StartStreamingRecognize), new HelpKeywordAttribute(""));
 
-            builder.AddCustomAttributes(typeof(StopStreamingRecognize), categoryAttribute);
+            builder.AddCustomAttributes(typeof(StopStreamingRecognize), categoryStreamingAttribute);
             builder.AddCustomAttributes(typeof(StopStreamingRecognize), new DesignerAttribute(typeof(StopStreamingRecognizeDesigner)));
             builder.AddCustomAttributes(typeof(StopStreamingRecognize), new HelpKeywordAttribute(""));
 
-            builder.AddCustomAttributes(typeof(ReadStreamingRecognize), categoryAttribute);
+            builder.AddCustomAttributes(typeof(ReadStreamingRecognize), categoryStreamingAttribute);
             builder.AddCustomAttributes(typeof(ReadStreamingRecognize), new DesignerAttribute(typeof(ReadStreamingRecognizeDesigner)));
             builder.AddCustomAttributes(typeof(ReadStreamingRecognize), new HelpKeywordAttribute(""));
-
+            #endregion
 
             MetadataStore.AddAttributeTable(builder.CreateTable());
         }
