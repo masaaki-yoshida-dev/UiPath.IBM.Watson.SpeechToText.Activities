33c7c2aa446be5fd64c550717430a2e2ccaf3fd5
Masaaki Yoshida <ma.yoshida.dev@gmail.com>
2020-04-21 23:33:57 +0900

    Exclude 3 activity which File category and ReadStreamingRecognize activity.

--- myoshidan.IBM.Watson.STT/myoshidan.IBM.Watson.STT.Activities.Design/DesignerMetadata.cs
+++ myoshidan.IBM.Watson.STT/myoshidan.IBM.Watson.STT.Activities.Design/DesignerMetadata.cs
@@ -15,20 +15,20 @@ namespace myoshidan.IBM.Watson.STT.Activities.Design
             var builder = new AttributeTableBuilder();
             builder.ValidateTable();
 
-            var categoryFileAttribute = new CategoryAttribute($"{Resources.CategoryFile}");
+            //var categoryFileAttribute = new CategoryAttribute($"{Resources.CategoryFile}");
             var categoryStreamingAttribute = new CategoryAttribute($"{Resources.CategoryStreaming}");
 
             #endregion Setup
 
-            #region FileGroup
-            builder.AddCustomAttributes(typeof(SpeechToTextFileScope), categoryFileAttribute);
-            builder.AddCustomAttributes(typeof(SpeechToTextFileScope), new DesignerAttribute(typeof(SpeechToTextFileScopeDesigner)));
-            builder.AddCustomAttributes(typeof(SpeechToTextFileScope), new HelpKeywordAttribute(""));
+            //#region FileGroup
+            //builder.AddCustomAttributes(typeof(SpeechToTextFileScope), categoryFileAttribute);
+            //builder.AddCustomAttributes(typeof(SpeechToTextFileScope), new DesignerAttribute(typeof(SpeechToTextFileScopeDesigner)));
+            //builder.AddCustomAttributes(typeof(SpeechToTextFileScope), new HelpKeywordAttribute(""));
 
-            builder.AddCustomAttributes(typeof(RecognizeAudioFile), categoryFileAttribute);
-            builder.AddCustomAttributes(typeof(RecognizeAudioFile), new DesignerAttribute(typeof(RecognizeAudioFileDesigner)));
-            builder.AddCustomAttributes(typeof(RecognizeAudioFile), new HelpKeywordAttribute(""));
-            #endregion
+            //builder.AddCustomAttributes(typeof(RecognizeAudioFile), categoryFileAttribute);
+            //builder.AddCustomAttributes(typeof(RecognizeAudioFile), new DesignerAttribute(typeof(RecognizeAudioFileDesigner)));
+            //builder.AddCustomAttributes(typeof(RecognizeAudioFile), new HelpKeywordAttribute(""));
+            //#endregion
 
             #region Streaming
             builder.AddCustomAttributes(typeof(SpeechToTextStreamingScope), categoryStreamingAttribute);
@@ -43,9 +43,9 @@ namespace myoshidan.IBM.Watson.STT.Activities.Design
             builder.AddCustomAttributes(typeof(StopStreamingRecognize), new DesignerAttribute(typeof(StopStreamingRecognizeDesigner)));
             builder.AddCustomAttributes(typeof(StopStreamingRecognize), new HelpKeywordAttribute(""));
 
-            builder.AddCustomAttributes(typeof(ReadStreamingRecognize), categoryStreamingAttribute);
-            builder.AddCustomAttributes(typeof(ReadStreamingRecognize), new DesignerAttribute(typeof(ReadStreamingRecognizeDesigner)));
-            builder.AddCustomAttributes(typeof(ReadStreamingRecognize), new HelpKeywordAttribute(""));
+            //builder.AddCustomAttributes(typeof(ReadStreamingRecognize), categoryStreamingAttribute);
+            //builder.AddCustomAttributes(typeof(ReadStreamingRecognize), new DesignerAttribute(typeof(ReadStreamingRecognizeDesigner)));
+            //builder.AddCustomAttributes(typeof(ReadStreamingRecognize), new HelpKeywordAttribute(""));
             #endregion
 
             MetadataStore.AddAttributeTable(builder.CreateTable());
